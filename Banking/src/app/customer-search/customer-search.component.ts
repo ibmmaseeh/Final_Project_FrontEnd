2c2fe7ae5508797ddf090bbc312684f5954f7645
unknown <ibmjavanh18@iiht.tech>
2021-05-06 00:10:52 +0530

    Customer Search

--- Banking/src/app/customer-search/customer-search.component.ts
+++ Banking/src/app/customer-search/customer-search.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Emp} from '../Emp'
 @Component({
   selector: 'app-customer-search',
   templateUrl: './customer-search.component.html',
   styleUrls: ['./customer-search.component.css']
 })
 export class CustomerSearchComponent implements OnInit {
-
+  empArray:Emp[]=[];
+  accountNumber:string='';
   constructor() { }
+search(){
 
+}
   ngOnInit(): void {
   }
 
