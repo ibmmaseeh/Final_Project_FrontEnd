6d3243ebe3d79d311bc08515a301091ae5e12ef2
unknown <ibmjavanh19@iiht.tech>
2021-05-05 22:09:00 +0530

    added update component

--- Banking/src/app/employee.service.ts
+++ Banking/src/app/employee.service.ts
@@ -8,19 +8,24 @@ const URL = 'http://localhost:8080/';
 })
 export class EmployeeService {
 
+  updateAccount(accountNumber: string, updatedBody: { firstName: string; lastName: string; houseNumber: string; city: string; state: string; pinCode: string; type: string; status: string; balance: string; accountNumber: string; }) {
+    const endpointURL = 'http://localhost:8080/employee/' + accountNumber;
+    return this.http.put(endpointURL, updatedBody);
+  }
+
   constructor(private http: HttpClient) { }
-  save(emp:Emp) {
-    return this.http.post((URL+'employee'),emp ,{
-      headers: {"content-type": 'application/json' },
+  save(emp: Emp) {
+    return this.http.post((URL + 'employee'), emp, {
+      headers: { "content-type": 'application/json' },
       responseType: "text"
     });
   }
-  getByAccountNumber(accountNumber:string  ){
-   
-    return this.http.get('http://localhost:8080/employee/'+accountNumber);
+  getByAccountNumber(accountNumber: string) {
+
+    return this.http.get('http://localhost:8080/employee/' + accountNumber);
   }
-   getAll(){
-     return this.http.get('http://localhost:8080/employee');
-}
+  getAll() {
+    return this.http.get('http://localhost:8080/employee');
+  }
 
 }
--- Banking/src/app/update-account/update-account.component.ts
+++ Banking/src/app/update-account/update-account.component.ts
@@ -1,15 +1,105 @@
 import { Component, OnInit } from '@angular/core';
-import {Emp} from '../Emp';
+import { Emp } from '../Emp';
 import { Address } from '../Address';
+import { EmployeeService } from '../employee.service';
 @Component({
   selector: 'app-update-account',
   templateUrl: './update-account.component.html',
   styleUrls: ['./update-account.component.css']
 })
 export class UpdateAccountComponent implements OnInit {
-  emp:Emp = new Emp();
-  address:Address=new Address();
-  constructor() { }
+  emp: Emp = new Emp();
+  address: Address = new Address();
+  empResult: any;
+  empArray: Emp[] = [];
+  accountNumber: string = '';
+  constructor(private employeeService: EmployeeService) { }
+
+  searchAccount() {
+    let endpointURL = 'http://localhost:8080/employee/';
+    let accountNumber = (<HTMLInputElement>document.getElementById('accountNumber')).value;
+    console.log(accountNumber);
+    if (accountNumber) {
+      endpointURL = endpointURL + accountNumber;
+      const promise = this.employeeService.getByAccountNumber(accountNumber);
+      promise.subscribe(response => {
+        this.empResult = response;
+        // console.log(this.empResult);
+        this.empArray = this.empResult;
+        // console.log(this.bugArray);
+        if (this.empArray){
+          this.emp = this.empArray[0];
+          console.log("1");
+          alert(" success");
+
+
+        }
+
+        else {
+          alert("Given Account with number " + accountNumber + " is not available ");
+        }
+      },
+        error => {
+          console.log(error);
+          alert("Error Happened!");
+        }
+      )
+    }
+    else {
+      alert("Specify Account Number to fetch Account details");
+
+    }
+  }
+
+
+  updateAccount() {
+    // let updateAccount= (<HTMLInputElement>document.getElementById('updateAccount'))
+    // if (!updateAccount.checkValidity()) {
+    //   alert('Form is Invalid! Please check whether all mandatory fields are filled!');
+    //   return;
+    // }
+    let accountNumber = (<HTMLInputElement>document.getElementById('accountNumber')).value
+    const updatedBody = {
+      firstName: (<HTMLInputElement>document.getElementById('fname')).value,
+      lastName: (<HTMLInputElement>document.getElementById('lname')).value,
+      houseNumber: (<HTMLInputElement>document.getElementById('houseNumber')).value,
+      city: (<HTMLInputElement>document.getElementById('city')).value,
+      state: (<HTMLInputElement>document.getElementById('state')).value,
+      pinCode: (<HTMLInputElement>document.getElementById('pincode')).value,
+      type: (<HTMLInputElement>document.getElementById('type')).value,
+      status: (<HTMLInputElement>document.getElementById('status')).value,
+      balance: (<HTMLInputElement>document.getElementById('balance')).value,
+      accountNumber: (<HTMLInputElement>document.getElementById('accountNumber')).value,
+    }
+
+
+    this.employeeService.updateAccount(accountNumber, updatedBody).subscribe(
+      response => {
+        console.log(response);
+        this.empResult = response;
+        console.log(response);
+        console.log(updatedBody.status);
+        alert('Account Details Updated');
+
+
+      },
+      error => {
+        console.log(error);
+        alert("Error Happened!");
+      }
+    )
+  }
+
+
+
+
+
+
+
+
+
+
+
 
   ngOnInit(): void {
   }
