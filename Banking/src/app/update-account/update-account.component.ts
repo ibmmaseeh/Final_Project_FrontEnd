25f5ec65b1a6d0e3852489d8c43c76a9d25aa65e
Aishwarya Srivastava <aishwaryaaaaaaaaaaa@gmail.com>
2021-05-05 13:36:45 +0530

    added UI

--- Banking/src/app/update-account/update-account.component.ts
+++ Banking/src/app/update-account/update-account.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Emp} from '../Emp';
 @Component({
   selector: 'app-update-account',
   templateUrl: './update-account.component.html',
   styleUrls: ['./update-account.component.css']
 })
 export class UpdateAccountComponent implements OnInit {
-
+  emp:Emp = new Emp();
   constructor() { }
 
   ngOnInit(): void {
